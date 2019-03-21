eaea1d2d8c28a91dc033dcf1fc6aafffe9433fb9
ilia10000 <isucholu@uwaterloo.ca>
2019-03-21 16:49:51 -0400

    proper library name

--- basics.py
+++ basics.py
@@ -34,7 +34,7 @@ def load_train_models(state):
         raise ValueError("train_nets_type: {}".format(state.train_nets_type))
 
 def cross_entropy(pred, soft_targets, **kwargs):
-    logsoftmax = nn.LogSoftmax()
+    logsoftmax = torch.nn.LogSoftmax()
     return torch.mean(torch.sum(- soft_targets * logsoftmax(pred), 1))
 def task_loss(state, output, label, **kwargs):
     if state.num_classes == 2:
