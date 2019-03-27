67b28448c415b92f80562ae853d44c9a546a7db8
ilia10000 <isucholu@uwaterloo.ca>
2019-03-27 00:24:36 -0400

    changed one hot func

--- train_distilled_image.py
+++ train_distilled_image.py
@@ -97,8 +97,8 @@ class Trainer(object):
         """
         req_lbl_grad = not self.state.static_labels
         k  = labels.size()[0]
-        labels_one_hot = torch.FloatTensor(k, num_classes).zero_()
-        target = labels_one_hot.scatter_(2, labels, 1, requires_grad=req_lbl_grad, device=self.state.device)
+        labels_one_hot = torch.FloatTensor(k, num_classes, requires_grad=req_lbl_grad, device=self.state.device).zero_()
+        target = labels_one_hot.scatter_(2, labels, 1)
     
             
         return target
