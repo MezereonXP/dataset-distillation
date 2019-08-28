d276e17edf43a1c0b3ee2e6e0f04f7786260d140
ilia10000 <isucholu@uwaterloo.ca>
2019-08-28 12:51:40 -0400

    Adding text

--- base_options.py
+++ base_options.py
@@ -533,9 +533,9 @@ class BaseOptions(object):
             test_dataset = datasets.get_dataset(state, 'test')
 
         if state.opt.textdata:
-            state.opt.train_loader = data.Iterator.splits(
+            state.opt.train_loader = data.Iterator(
         train_dataset, batch_size=state.batch_size, device=state.device, repeat=False, sort_key=lambda x: len(x.train_dataset))
-            state.opt.test_loader = data.Iterator.splits(
+            state.opt.test_loader = data.Iterator(
         test_dataset, batch_size=state.test_batch_size, device=state.device, repeat=False, sort_key=lambda x: len(x.test_dataset))
         else:
             state.opt.train_loader = torch.utils.data.DataLoader(
