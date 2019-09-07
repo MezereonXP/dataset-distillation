6c278c1a1dd144ed95fb2dc358bda6a63e980202
ilia10000 <isucholu@uwaterloo.ca>
2019-09-07 02:12:49 -0400

    Adding SST-5

--- networks/networks.py
+++ networks/networks.py
@@ -124,7 +124,7 @@ class TextConvNet3(utils.ReparamModule):
         n_filters = 100
         filter_sizes = [3,4,5]
         dropout=0.5
-        output_dim=1
+        output_dim=1 if state.num_classes == 2 else state.num_classes
         self.encoder = nn.Embedding(ntoken, embedding_dim)
         self.encoder.weight.data.copy_(state.pretrained_vec) # load pretrained vectors
         self.encoder.weight.requires_grad = state.learnable_embedding 
@@ -156,55 +156,6 @@ class TextConvNet3(utils.ReparamModule):
         cat = self.dropout(torch.cat(pooled, dim = 1))
         
         return self.fc(cat)     
-class CNN(nn.Module):
-    
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
-                 dropout, pad_idx):
-        
-        super().__init__()
-                
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
-        
-        self.convs = nn.ModuleList([
-                                    nn.Conv2d(in_channels = 1, 
-                                              out_channels = n_filters, 
-                                              kernel_size = (fs, embedding_dim)) 
-                                    for fs in filter_sizes
-                                    ])
-        
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
-        
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, text):
-        
-        #text = [sent len, batch size]
-        
-        text = text.permute(1, 0)
-                
-        #text = [batch size, sent len]
-        
-        embedded = self.embedding(text)
-                
-        #embedded = [batch size, sent len, emb dim]
-        
-        embedded = embedded.unsqueeze(1)
-        
-        #embedded = [batch size, 1, sent len, emb dim]
-        
-        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-            
-        #conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
-                
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        
-        #pooled_n = [batch size, n_filters]
-        
-        cat = self.dropout(torch.cat(pooled, dim = 1))
-
-        #cat = [batch size, n_filters * len(filter_sizes)]
-            
-        return self.fc(cat)    
     
 class AlexCifarNet(utils.ReparamModule):
     supported_dims = {32}
