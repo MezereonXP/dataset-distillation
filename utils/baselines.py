55a11c3a627c082c40b10d4a1adadee6a0274093
ilia10000 <isucholu@uwaterloo.ca>
2019-09-30 23:02:16 -0400

    Fixing labels for baseline tests

--- utils/baselines.py
+++ utils/baselines.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 
 def encode(d, state):
-    encoder = nn.Embedding(state.ntoken, state.ninp)
+    encoder = nn.Embedding(state.ntoken, state.ninp).to(state.device)
     encoder.weight.data.copy_(state.pretrained_vec) # load pretrained vectors
     encoder.weight.requires_grad = False
     return encoder(d)
@@ -38,7 +38,7 @@ def random_train(state):
         for data, label in zip(datas, labels):
             data=data.to(state.device, non_blocking=True)
             if state.textdata:
-                data=encode(data, state).to(state.device)
+                data=encode(data, state)
             label_id = label.item()
             if counts[label_id] < needed:
                 counts[label_id] += 1
