415e3e3f81ca7c2b3d6089c788433862ae908731
Ilia <isucholu@uwaterloo.ca>
2019-12-11 20:48:13 -0500

    updated command for random network inits

--- gen_exps.py
+++ gen_exps.py
@@ -13,7 +13,7 @@ epochs=50
 exps={}
 exps["dataset"]={"MNIST", "Cifar10"}
 exps["softmax"]={1,0}
-exps["network_init"]={"Fixed"}#,"Random"}
+exps["network_init"]={"Random"}#,"Fixed"}
 exps["label_init"]={"zeros", "uniform", "stdnormal", "bin", "hard", "smoothed", "orthogonal", "CNDB", "CNIDB", "AIDB-MSE","AIDB-SSIM", "AIIDB-MSE", "AIIDB-SSIM"}
 exps["repl"]={1}#,2,3}
 exps["add"]={0,-1}
@@ -44,7 +44,7 @@ def write_to_batch_files(batch_size=40, devices=8):
     for combo in combos:
         with open("exp_scripts/label_exps_{0}.sh".format(int(np.floor(i/batch_size))),"a+") as f:
             command = "python3 main.py --mode distill_basic --dataset {0} --arch {1}Net {2} --distill_steps 1 --static_labels 0 --random_init_labels {3} --distill_lr 0.01  --decay_epochs 35 --epochs {12} --lr 0.01  --results_dir {4} --device_id {11} --add_label_scaling {5} --mult_label_scaling {6} --dist_metric {7} --invert_dist {8} --label_softmax {9} > exp_scripts/batch_output_{10} 2>&1 &"
-            random_init = "--train_nets_type known_init --n_nets 1 --test_nets_type same_as_train" if combo[2] == "Fixed" else ""
+            random_init = "--train_nets_type known_init --n_nets 1 --test_nets_type same_as_train" if combo[2] == "Fixed" else "--test_n_nets 200"
             network = "Le" if combo[0]=="MNIST" else "AlexCifar"
             dist_metric = "SSIM" if "SSIM" in combo[3] else "MSE"
             invert_dist = 1 if ("CNIDB" in combo[3] or "AIIDB" in combo[3]) else "''"
